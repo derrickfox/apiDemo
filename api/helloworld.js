cde46a0ce2219ea8c0b8b645e612e6053e8a56c9
Derrick Fox <derrick.fox2@nih.gov>
2017-06-23 12:17:35 -0400

    Lastest

---
+++ api/helloworld.js
@@ -0,0 +1,9 @@
+var helloworld = module.exports;
+
+function hello(request, response, next) {
+    response.send('Hello world!');
+}
+
+helloService.routes = [
+    { path: '/hello', httpMethod: 'GET', middleware: hello }
+]
